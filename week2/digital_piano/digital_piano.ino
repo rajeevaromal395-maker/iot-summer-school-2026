1a6249f16595d4d364241ace4ab841c0c5ceecbc
rajeevaromal395-maker <rajeevaromal395@gmail.com>
2026-07-05 22:27:57 +0530

    Update digital_piano.ino
    feat: implement basic 4-key piano functionality

--- week2/digital_piano/digital_piano.ino
+++ week2/digital_piano/digital_piano.ino
@@ -1,1 +1,7 @@
-
+void loop() {
+  if (digitalRead(8)) tone(5, 262);
+  else if (digitalRead(9)) tone(5, 294);
+  else if (digitalRead(10)) tone(5, 330);
+  else if (digitalRead(11)) tone(5, 349);
+  else noTone(5);
+}
