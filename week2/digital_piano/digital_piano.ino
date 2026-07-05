8f573a48ef7313245f72ef8091feda3a22cc65d3
rajeevaromal395-maker <rajeevaromal395@gmail.com>
2026-07-05 22:31:03 +0530

    Update digital_piano.ino
    feat: add mode toggle for major/minor scales

--- week2/digital_piano/digital_piano.ino
+++ week2/digital_piano/digital_piano.ino
@@ -5,3 +5,10 @@ void loop() {
   else if (digitalRead(11)) tone(5, 349);
   else noTone(5);
 }
+bool isMajor = true;
+// Inside loop():
+if (digitalRead(12) == LOW) { // Mode toggle button
+  isMajor = !isMajor;
+  delay(250); // Simple debounce
+}
+// Use isMajor to pick between majorNotes[] and minorNotes[]
