e1e52848f217449b0fa76549342d7105e5c3a5d1
rajeevaromal395-maker <rajeevaromal395@gmail.com>
2026-07-05 22:22:20 +0530

    Update traffic_light.ino
    feat: add traffic light controller code.

--- week2/traffic_light/traffic_light.ino
+++ week2/traffic_light/traffic_light.ino
@@ -1,1 +1,45 @@
+// Define Pins
+const int redPin = 2;
+const int yellowPin = 3;
+const int greenPin = 4;
+const int buttonPin = 7;
 
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP); // Using internal pull-up
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Check for Pedestrian Button
+  if (digitalRead(buttonPin) == LOW) {
+    handlePedestrian();
+  }
+
+  // Traffic Light Sequence
+  lightState("RED", redPin, 5000);
+  lightState("YELLOW", yellowPin, 2000);
+  lightState("GREEN", greenPin, 4000);
+}
+
+void lightState(String color, int pin, int duration) {
+  digitalWrite(redPin, LOW);
+  digitalWrite(yellowPin, LOW);
+  digitalWrite(greenPin, LOW);
+  
+  digitalWrite(pin, HIGH);
+  Serial.print(millis());
+  Serial.print(" ms: Light State -> ");
+  Serial.println(color);
+  delay(duration);
+}
+
+void handlePedestrian() {
+  Serial.println("Pedestrian button pressed! Forcing RED.");
+  digitalWrite(redPin, HIGH);
+  digitalWrite(yellowPin, LOW);
+  digitalWrite(greenPin, LOW);
+  delay(8000);
+}
